1e2bd50ba260e50a41a093b7b01dbfa514430d5d
Fer Martin <fernandomartinsantana@gmail.com>
2014-10-30 17:59:34 +0800

    Adding authentication with devise

--- Gemfile
+++ Gemfile
@@ -73,3 +73,4 @@ end
 # Use debugger
 # gem 'debugger', group: [:development, :test]
 
+gem 'devise'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :weeks
 
   get '/weeks_table' => "weeks#weeks_table"
